1ae863974b3ba83ba67695561dd7b5b58dab0495
yusuff-fatorisa <bosun4real009@gmail.com>
2023-02-11 08:56:02 -0800

    fix pycodestyle error in base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -29,9 +29,7 @@ class BaseModel(object):
         if kwargs:
             for key, val in kwargs.items():
                 if key in ["created_at, updated_at"]:
-                    #fmt = "%Y-%m-%dT%H:%M:%S.%f"
                     val = dt.strptime(val, "%Y-%m-%dT%H:%M:%S.%f")
-                    #setattr(self, key, time)
                 elif key != "__class__":
                     setattr(self, key, val)
                 else:
@@ -42,17 +40,18 @@ class BaseModel(object):
         return f"{[self.__class__.__name__]} {(self.id)} {self.__dict__}"
 
     def save(self):
-        """updates the public instance attribute 
+        """updates the public instance attribute
            'updated_at' with the current datetime.
         """
         self.updated_at = datetime.datetime.now()
 
     def to_dict(self):
         """
-        returns a dictionary containing all keys/values of 
+        returns a dictionary containing all keys/values of
         '__dict__' of the instance or object.
         """
+        fmt = "%Y-%m-%dT%H:%M:%S.%f"
         self.__dict__["__class__"] = self.__class__.__name__
-        self.__dict__["created_at"] = self.created_at.strftime(("%Y-%m-%dT%H:%M:%S.%f"))
-        self.__dict__["updated_at"] = self.updated_at.strftime(("%Y-%m-%dT%H:%M:%S.%f"))
+        self.__dict__["created_at"] = self.created_at.strftime(fmt)
+        self.__dict__["updated_at"] = self.updated_at.strftime(fmt)
         return self.__dict__
