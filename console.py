2e375caa6688cf72b9b33c5bb4a091091fdb5f5d
Hilary Titus Naor <hilarytn@gmail.com>
2023-02-13 03:38:55 -0800

    Fix pycodestyle indentation

--- console.py
+++ console.py
@@ -1,9 +1,14 @@
 #!/usr/bin/python3
+"""Defines the HBnB console"""
 import cmd
 from models.base_model import BaseModel
 
 
 class HBNBCommand(cmd.Cmd):
+    """Defines the HolbertonBnB command interpreter.
+        Attributes:
+        prompt (str): The command prompt.
+        """
     prompt = "(hbnb) "
 
     def do_quit(self, line):
