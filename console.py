160bd5f49e89758d04e3dc1e48eb4488ef5381f5
Hilary Titus Naor <hilarytn@gmail.com>
2023-02-13 03:32:41 -0800

    Remove empty methods because of checker

--- console.py
+++ console.py
@@ -19,32 +19,6 @@ class HBNBCommand(cmd.Cmd):
         """Do nothing at empty line"""
         pass
 
-    def do_print(self, line):
-        """Print Hilary and Yusuff"""
-        print("Hilary and Yusuff")
-
-    def do_create(self, line):
-        """Create an user"""
-        user = BaseModel()
-        print(user.id, user.created_at, user.updated_at)
-
-    def do_show(self, line):
-        """ Prints the string representation of
-        an instance based on the class name and id"""
-
-    def do_destroy(self, line):
-        """ Deletes an instance based on the class
-        name and id (save the change into the JSON file)"""
-
-    def do_all(self, line):
-        """Prints all string representation of all
-        instances based or not on the class name"""
-
-    def do_update(self, line):
-        """Updates an instance based on the class
-        name and id by adding or updating attribut
-        e (save the change into the JSON file)"""
-
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
